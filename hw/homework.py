a9408c097d60894af3d45c4e8f668c574f8604b8
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-02-23 16:09:09 +0200

    homework done

---
+++ hw/homework.py
@@ -0,0 +1,20 @@
+# 1.
+# 1) Get user name
+# 2) Greet user by name
+
+# 2.
+# 1) Get user email
+# 2) Print user email into the console
+
+
+# 3.
+# 1.) Get user year of birth
+# 2.) Print year of birth to the console
+
+# user_name = input("Eneter your name : ")
+
+# f''
+# print("Hello " + 2)
+# print(f"Hello {user_name}")
+# while True:
+#     print("Huck!")
